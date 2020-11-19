1805f3783ba406eee9242fe447455a10c007dc41
Yusnel Rojas Garcia <yusnel-rojas@users.noreply.github.com>
2020-11-18 22:19:24 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import { upload } from './upload'
+import { upload, fileDelete } from './upload'
 
 const accessToken = core.getInput('dropbox_access_token')
 const file = core.getInput('file')
 const path = core.getInput('path')
+const overwrite = core.getInput('overwrite') == 'yes'
 
 async function run() {
   try {
     const contents = await fs.promises.readFile(file)
+    
+    if (overwrite){
+      await fileDelete(path);
+    }catch (error) {
+      core.info(`Error deleting the file '${path}'`)
+    }
+    
     core.debug(`Uploading '${file}' -> '${path}'`)
     await upload(path, contents, accessToken)
     core.info(`'${file}' -> '${path}' has been successfully uploaded`)
